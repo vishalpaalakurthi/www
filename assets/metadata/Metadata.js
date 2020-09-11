690c326f85167cdb3d8eb74e6b249a31061955bb
Vishal Paalakurthi <vishalpaalakurthi@gmail.com>
2020-09-11 11:42:57 +0000

    blog added

--- assets/metadata/Metadata.js
+++ assets/metadata/Metadata.js
@@ -2,7 +2,7 @@
 export var RecentArticles = [{
     title: "Build your own simple cassandra workbench",
     shortNote: "This workbench is to skip the terminal for simple queries",
-    publishedDate: "2020-09-08",
+    publishedDate: "2020-09-11",
     author: "Vishal Paalakurthi",
-    path: "blog/chrome"
+    path: "blog/build_your_own_Cassandra_workbench"
 }]
