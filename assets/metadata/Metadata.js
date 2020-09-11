64276d23565924becdceba2e90633727dcb084f5
Vishal Paalakurthi <vishalpaalakurthi@gmail.com>
2020-09-11 11:51:50 +0000

    fix mdx page

--- assets/metadata/Metadata.js
+++ assets/metadata/Metadata.js
@@ -4,5 +4,5 @@ export var RecentArticles = [{
     shortNote: "This workbench is to skip the terminal for simple queries",
     publishedDate: "2020-09-11",
     author: "Vishal Paalakurthi",
-    path: "blog/build_your_own_Cassandra_workbench"
+    path: "blog/build_your_own_cassandra_workbench"
 }]
