4f3c6c8c1cdd6f90cfd50121dedad76ba79ae5fe
Vishal Paalakurthi <30443350+vishalpaalakurthi@users.noreply.github.com>
2020-09-16 12:15:35 +0530

    Update Metadata.js

--- assets/metadata/Metadata.js
+++ assets/metadata/Metadata.js
@@ -1,5 +1,11 @@
 
 export var RecentArticles = [{
+    title: "Why coloring terminal is very important!",
+    shortNote: "Add background color to your terminal to avoid negative impact.",
+    publishedDate: "2020-09-16",
+    author: "Vishal Paalakurthi",
+    path: "blog/why_coloring_terminal_is_very_important"
+},{
     title: "Build your own simple cassandra workbench",
     shortNote: "This workbench is to skip the terminal for simple queries",
     publishedDate: "2020-09-11",
@@ -8,6 +14,12 @@ export var RecentArticles = [{
 }]
 
 export var Articles = [{
+    title: "Why coloring terminal is very important!",
+    shortNote: "Add background color to your terminal to avoid negative impact.",
+    publishedDate: "2020-09-16",
+    author: "Vishal Paalakurthi",
+    path: "blog/why_coloring_terminal_is_very_important"
+},{
     title: "Build your own simple cassandra workbench",
     shortNote: "This workbench is to skip the terminal for simple queries",
     publishedDate: "2020-09-11",
